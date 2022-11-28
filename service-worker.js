0935fc04b0e2ba364c99ba335dbf30c41cdafc5e
aqilahshi <75949194+aqilahshi@users.noreply.github.com>
2022-11-28 18:25:22 +0000

    Deploying to gh-pages from  @ 66099885a9da1b5af9e27c33064f65734fc47bed 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c33ab944d7605e0ce125c99d0bd0d063.js"
+  "/precache-manifest.37897591635d0641ea6714f754cda3c2.js"
 );
 
 self.addEventListener('message', (event) => {
