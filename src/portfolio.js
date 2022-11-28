f1b7bedd59288df7c59c1c2bbabea4de1614fd21
aqilahshi <75949194+aqilahshi@users.noreply.github.com>
2022-11-28 17:15:10 +0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -346,8 +346,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "016-4429436",
+  email_address: "aqilahera@gmail.com"
 };
 
 // Twitter Section
